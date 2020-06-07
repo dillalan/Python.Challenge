4cb66015f4f47e3490bcb8f63812fc242e2f33d3
Alan Dill <alandill@rocketmail.com>
2020-06-07 19:51:18 -0300

    Challenge 2 in pythonchallenge.com

---
+++ PythonChallenge2.py
@@ -0,0 +1,13 @@
+import urllib.request
+from collections import Counter
+
+response = urllib.request.urlopen('http://www.pythonchallenge.com/pc/def/ocr.html')
+html = response.read()
+text = str(html)
+soup = text.split()
+code = soup[-1]
+code = code[6:]
+r = Counter(code).most_common()
+print(r[-10:])
+
+# resposta = equality
